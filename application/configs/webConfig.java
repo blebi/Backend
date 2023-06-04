1583108e10fbfb35f6d6954e9e384e11fa962520
Texelo <53214117+Texelo@users.noreply.github.com>
2023-06-04 02:35:20 +0100

    initial commit

---
+++ application/configs/webConfig.java
@@ -0,0 +1,17 @@
+package application.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+
+@Configuration
+public class webConfig {
+  @Bean
+  public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(10);
+    executor.setThreadNamePrefix("async-");
+    return executor;
+  }
+}
---
+++ application/controllers/CustomErrorController.java
@@ -0,0 +1,13 @@
+package application.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+  @GetMapping("/error")
+  public String error() {
+    return "404";
+  }
+}
